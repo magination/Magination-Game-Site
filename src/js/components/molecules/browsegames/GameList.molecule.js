047534c5ef158915a5c93f40e3ed7242be1d190f
Simen Vie <simenvie@gmail.com>
2016-03-19 16:42:22 +0100

    browse games fetches actual games from api, still needs error handlong

--- src/js/components/molecules/browsegames/GameList.molecule.js
+++ src/js/components/molecules/browsegames/GameList.molecule.js
@@ -11,6 +11,11 @@ var GameList = React.createClass({
 			games: this.props.games
 		};
 	},
+	componentWillReceiveProps: function(nextProps){
+		this.setState({
+			games: nextProps.games
+		});
+	},
 	render: function(){
 		var games = this.state.games.map(function(game) {
       		return <li className="list-group-item" key={game._id}><Game title={game.title} description={game.description} owner={game.owner}></Game></li>;
--- src/js/components/organisms/BrowseGames.organism.js
+++ src/js/components/organisms/BrowseGames.organism.js
@@ -1,6 +1,6 @@
 var React = require('react');
 
-var urls = require('../../config/config').urls;
+var URLS = require('../../config/config').urls;
 
 var GameList = require('../molecules/browsegames/GameList.molecule');
 
@@ -9,18 +9,23 @@ var BrowseGames = React.createClass({
 		{title:"Game1", description:"this game is shit", _id:"123", owner:"Simen"},
 		{title:"Game2", description:"this game is shitter actually", _id:"12", owner:"Petter"}
 	],
+	getInitialState: function(){
+		return {
+			games: []
+		}
+	},
 	componentWillMount: function(){
 		$.ajax({
 			type: "GET",
-		   	url: urls.api.games,
+		   	url: URLS.api.games,
 		   	dataType: "json",
-		   	success: this.didRecieveData
+		   	success: this.didRecieveData /*TODO needs error handling*/
 		});
 	},
 	render: function(){
 		return (
 			<div>
-				<GameList games={this.gamesData}/>
+				<GameList games={this.state.games}/>
 			</div>
 		);
 	},
