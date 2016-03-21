512dcb4db8d884329e240f964e2d6da5895b24e2
Simen Vie <simenvie@gmail.com>
2016-03-21 22:34:26 +0100

    added url navigation, /browse now responds with the browse page

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ var app = express();
 
 app.use('/public', express.static(path.join(__dirname, '../build')));
 
-app.get('/', function(req,res){
+var possiblePaths = ['/', '/browse', '/login', 'upload'];
+app.get(possiblePaths, function(req,res){
 	res.sendFile('index.html', { root: path.join(__dirname, '../src') });
 });
 
