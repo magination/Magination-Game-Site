e04b3a93d9dfba49c4447b893ed99b2da689388a
Simen Vie <simenvie@gmail.com>
2016-03-21 21:11:43 +0100

    bug fix - logout now shows pointer, added relevant names to profile dropdown (still unfunctional)

--- src/js/components/molecules/navigation/NavigationLoggedInState.molecule.js
+++ src/js/components/molecules/navigation/NavigationLoggedInState.molecule.js
@@ -24,20 +24,18 @@ var NavigationLoggedInState = React.createClass({
 				<li className="dropdown">
           		<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{this.state.email} <span className="caret"></span></a>
 	          		<ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
+			            <li><a href="#">My Profile</a></li>
+			            <li><a href="#">My Games</a></li>
 			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
+			            <li><a href="#">Settings</a></li>
 			            <li role="separator" className="divider"></li>
-			            <li><a onClick={this._onLogoutClicked}>Logout</a></li>
+			            <li><a href="#" onClick={this._onLogoutClicked}>Logout</a></li>
 			        </ul>
 		        </li>
 			</ul>
 		)
 	},
 	_onLogoutClicked: function(){
-		console.log('Hei');
 		LoginAction.logoutSuccess();
 	}
 });
