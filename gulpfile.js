98495f2fcd2233c30f5bdbce5b1c1364b416c118
Simen Vie <simenvie@gmail.com>
2016-04-06 10:13:10 +0200

    fixed gulp not reloading page after change in build

--- gulpfile.js
+++ gulpfile.js
@@ -4,57 +4,49 @@ var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
-var watchify = require('watchify');
 var babel = require('babelify');
 var nodemon = require('gulp-nodemon');
 var del = require('del');
 var browserSync = require('browser-sync');
 
+function reloadBrowser (delay) {
+	setTimeout(function () {
+		browserSync.reload();
+	}, delay);
+}
+
 function compile (src, dst) {
-	var bundler = browserify(
-		src
-	)
+	var bundler = browserify(src)
 	.transform(babel.configure({
 		presets: ['react']
 	}));
 
-	bundler.bundle()
+	return bundler.bundle()
 		.on('error', function (err) { console.error(err); this.emit('end'); })
 		.pipe(source('app.js'))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dst));
-	return;
 }
 
-gulp.task('build', ['lint'], function() {
-	console.log('building');
-	return compile('./src/js/RenderApp.js','./build');
+gulp.task('build', ['lint'], function () {
+	return compile('./src/js/RenderApp.js', './build');
 });
 
-gulp.task('watch', function() {
-	//gulp.watch('src/**/*.js', ['build']);
-	gulp.watch('build/**', function () {
-  		setTimeout(function () {
-  			browserSync.reload();
-  		}, 1000);
-	});
+gulp.task('watch', function () {
+	gulp.watch('src/**/*.js', ['build']);
 });
 
-gulp.task('server', ['clean'], function () {
-	compile();
+gulp.task('server', ['clean', 'build'], function () {
+	// compile('./src/js/RenderApp.js', './build');
 	nodemon({
-	    script: './server/server.js',
+		script: './server/server.js',
 		ext: 'js html',
-		ignore: ['build/**'],
-		env: { 'NODE_ENV': 'development' },
-		tasks: function (changedFiles) {
-			return ['build'];
-		}
+		ignore: ['src/**, gulpfile.js'],
+		env: { 'NODE_ENV': 'development' }
 	}).on('restart', function () {
-		console.log('reloading');
-		browserSync.reload();
+		reloadBrowser(1000);
 	});
 });
 
@@ -70,15 +62,16 @@ gulp.task('clean', function () {
 });
 
 gulp.task('test', function () {
-	compile('./src/js/App', './test');
+	/* test is not yet implemented */
+	// compile('./src/js/App', './test');
 });
 
-gulp.task('browserSync', function () {
+gulp.task('default', ['watch', 'server'], function () {
+	/* What happens after everything is build and server is running in dev mode */
 	browserSync({
-        proxy: {
-        	target: 'http://localhost:8080'
-        }
-    });
+		proxy: {
+			target: 'http://localhost:8080'
+		}
+	});
+	browserSync.pause();
 });
-
-gulp.task('default', ['watch', 'browserSync', 'server']);
