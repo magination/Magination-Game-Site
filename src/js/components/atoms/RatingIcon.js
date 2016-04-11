8ddf0882bf8f9d202cf88a87ce2eae8191b88a22
Petter <iversen332@gmail.com>
2016-04-11 14:55:34 +0200

    added molecule and icon for rating

---
+++ src/js/components/atoms/RatingIcon.js
@@ -0,0 +1,15 @@
+/**
+ * Created by petteriversen on 11.04.16.
+ */
+var React = require('react');
+var Glyphicon = require('react-bootstrap').Glyphicon;
+
+var RatingIcon = React.createClass({
+	render: function () {
+		return (
+			<Glyphicon glyph={this.props.isSelected ? this.props.selectedImage : this.props.unselectedImage} onMouseOver={this.props.onMouseOver} onMouseLeave={this.props.onMouseLeave} onClick={this.props.onClick}
+			/>
+		);
+	}
+});
+module.exports = RatingIcon;
