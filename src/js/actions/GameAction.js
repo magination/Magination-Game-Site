b23186c75ec86a19b3d1a29854cc10f76e67d7c6
Petter Iversen <iversen332@gmail.com>
2016-04-21 23:07:01 +0200

    refactor

--- src/js/actions/GameAction.js
+++ src/js/actions/GameAction.js
@@ -1,23 +1,23 @@
 var Dispatcher = require('../dispatchers/Dispatcher');
-var EditGameConstants = require('../constants/EditGameConstants');
+var GameConstants = require('../constants/GameConstants');
 
 var GameAction = {
 	storeGameToServer: function (data) {
 		Dispatcher.dispatch({
-			actionType: EditGameConstants.STORE_GAME_TO_SERVER,
+			actionType: GameConstants.STORE_GAME_TO_SERVER,
 			game: data.game
 		});
 	},
 	updateCurrentGameLocally: function (data) {
 		Dispatcher.dispatch({
-			actionType: EditGameConstants.UPDATE_GAME_LOCALLY,
+			actionType: GameConstants.UPDATE_GAME_LOCALLY,
 			propertyName: data.propertyName,
 			propertyValue: data.propertyValue
 		});
 	},
 	createNewGameLocally: function () {
 		Dispatcher.dispatch({
-			actionType: EditGameConstants.CHANGE_GAME_LOCALLY,
+			actionType: GameConstants.CHANGE_GAME_LOCALLY,
 			game: {}
 		});
 	}
