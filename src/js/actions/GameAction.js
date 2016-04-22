f8710839d49b47db537ecfd95db7d5f616280f6c
Petter Iversen <iversen332@gmail.com>
2016-04-22 11:07:01 +0200

    fix gameaction

--- src/js/actions/GameAction.js
+++ src/js/actions/GameAction.js
@@ -1,12 +1,33 @@
 var Dispatcher = require('../dispatchers/Dispatcher');
 var GameConstants = require('../constants/GameConstants');
 
+ var URLS = require('../config/config').urls;
+ var LoginStore = require('./loginStore');
+ var FeedbackAction = require('../../actions/FeedbackAction');
+
 var GameAction = {
 	storeGameToServer: function (data) {
-		Dispatcher.dispatch({
-			actionType: GameConstants.STORE_GAME_TO_SERVER,
-			game: data.game
-		});
+		 $.ajax({
+		 type: 'POST',
+		 url: URLS.api.games,
+		 data: JSON.stringify({
+		 game: data.game
+		 }),
+		 headers: {
+		 'Authorization': LoginStore.getToken()
+		 },
+		 contentType: 'application/json',
+		 dataType: 'json',
+		 statusCode: {
+		 200: onGamePostedSuccess,
+		 401: onPostGameUnauthorizedResponse,
+		 500: function () {
+		 alert('Server Error: see console');
+		 console.log(data);
+		 }
+		 }
+		 });
+
 	},
 	updateCurrentGameLocally: function (data) {
 		Dispatcher.dispatch({
@@ -22,5 +43,19 @@ var GameAction = {
 		});
 	}
 };
+/*
+ var onGamePostedSuccess = function (data) {
+ Dispatcher.dispatch({
+ actionType: GameConstants.CHANGE_GAME_LOCALLY,
+ game: data.game
+ });
+ };
+ var onPostGameUnauthorizedResponse = function () {
+ FeedbackAction.displayWarningMessage({
+ header: 'Not signed in.',
+ message: 'Please sign in to upload a game.'
+ });
+ };
+ */
 
 module.exports = GameAction;
