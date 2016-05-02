2ef05dd5a2bb63f95cd1e98b7b4d69ad845b014d
Simen Vie <simenvie@gmail.com>
2016-05-02 12:52:08 +0200

    bug fixes and tuning on image carousel

--- src/js/components/molecules/game/ImageCarousel.molecule.js
+++ src/js/components/molecules/game/ImageCarousel.molecule.js
@@ -8,29 +8,32 @@ var ImageCarousel = React.createClass({
 	getInitialState: function () {
 		return {
 			index: 0,
-			width: '10',
-			height: '10'
+			width: '400',
+			height: '400'
 		};
 	},
 	componentWillReceiveProps: function (nextProps) {
-		var width = ReactDOM.findDOMNode(this).offsetWidth + 10;
-		var height = nextProps.height - 10;
+		var width = ReactDOM.findDOMNode(this).offsetWidth;
+		var border = 10;
+		var fontSize = 50;
+		var height = nextProps.height;
 		var innerDivStyle = {
-			width: (height > width) ? width : height,
-			height: (height > width) ? width : height,
+			width: (height > width) ? (width - border) : (height - border),
+			height: (height > width) ? (width - border) : (height - border),
 			margin: 'auto'
 		};
 		var chevronRightStyle = $.extend(true, {}, ContainerStyles.imgCarouselChevronContainer);
-		chevronRightStyle.left = ((width * 80) / 100);
-		chevronRightStyle.width = ((20 * width) / 100);
-		chevronRightStyle.height = height;
-		chevronRightStyle.paddingTop = (height / 2) - 50;
+		chevronRightStyle.left = ((width * 85) / 100) + border;
+		chevronRightStyle.width = ((15 * width) / 100);
+		chevronRightStyle.height = height - border;
+		chevronRightStyle.paddingTop = (height / 2) - fontSize;
 		var chevronLeftStyle = $.extend(true, {}, ContainerStyles.imgCarouselChevronContainer);
-		chevronLeftStyle.width = ((20 * width) / 100);
-		chevronLeftStyle.height = height;
-		chevronLeftStyle.paddingTop = (height / 2) - 50;
+		chevronLeftStyle.width = ((15 * width) / 100);
+		chevronLeftStyle.height = height - border;
+		chevronLeftStyle.paddingTop = (height / 2) - fontSize;
 		var divStyle = $.extend(true, {}, ContainerStyles.imgCarouselContainer);
 		divStyle.height = nextProps.height;
+		divStyle.textAlign = 'center';
 		this.setState({
 			width: width,
 			height: height,
@@ -38,8 +41,47 @@ var ImageCarousel = React.createClass({
 			divStyle: divStyle,
 			chevronRightStyle: chevronRightStyle,
 			chevronLeftStyle: chevronLeftStyle
-
 		});
+	},
+	componentDidMount: function () {
+		$(window).resize(this.onResizeEvent);
+	},
+	componentWillUnmount: function () {
+		$(window).unbind('resize');
+	},
+	render: function () {
+		var stateIndex = this.state.index;
+		var carouselItems = this.props.imageUrls.map(function (url, index) {
+			var style = {
+				maxHeight: '100%',
+				maxWidth: '80%',
+				minWidth: '80%',
+				minHeight: '80%',
+				marginRight: 'auto',
+				marginLeft: 'auto',
+				WebkitTransform: 'translate(0,10%)'
+			};
+			if (index !== stateIndex) {
+				style.display = 'none';
+			}
+			return <img key={url} style={style} alt='There was an error loading the image' src={url}/>;
+		});
+		return (
+			<div style={this.state.divStyle}>
+				<div hidden={this.state.index === 0} onClick={this.selectPreviousImage} style={this.state.chevronLeftStyle}>
+					<h1 style={TextStyles.chevronGlyph}><Glyphicon glyph='chevron-left'/></h1>
+				</div>
+				<div hidden={this.state.index === this.props.imageUrls.length - 1} onClick={this.selectNextImage} style={this.state.chevronRightStyle}>
+					<h1 style={TextStyles.chevronGlyph}><Glyphicon glyph='chevron-right'/></h1>
+				</div>
+				<div style={this.state.innerDivStyle}>
+					{carouselItems}
+				</div>
+			</div>
+		);
+	},
+	onResizeEvent: function () {
+		this.componentWillReceiveProps(this.props);
 	},
 	selectNextImage: function () {
 		if (this.state.index + 1 >= this.props.imageUrls.length) {
@@ -56,34 +98,6 @@ var ImageCarousel = React.createClass({
 		this.setState({
 			index: this.state.index - 1
 		});
-	},
-	render: function () {
-		var stateIndex = this.state.index;
-		var carouselItems = this.props.imageUrls.map(function (url, index) {
-			var style = {
-				maxHeight: '100%',
-				maxWidth: '100%',
-				minHeight: '100%',
-				minWidth: '100%'
-			};
-			if (index !== stateIndex) {
-				style.display = 'none';
-			}
-			return <img key={url} style={style} alt='There was an error loading the image' src={url}/>;
-		});
-		return (
-			<div style={this.state.divStyle}>
-				<div onClick={this.selectPreviousImage} style={this.state.chevronLeftStyle}>
-					<h1 style={TextStyles.chevronGlyph}><Glyphicon glyph='chevron-left'/></h1>
-				</div>
-				<div onClick={this.selectNextImage} style={this.state.chevronRightStyle}>
-					<h1 style={TextStyles.chevronGlyph}><Glyphicon glyph='chevron-right'/></h1>
-				</div>
-				<div style={this.state.innerDivStyle}>
-					{carouselItems}
-				</div>
-			</div>
-		);
 	}
 });
 
