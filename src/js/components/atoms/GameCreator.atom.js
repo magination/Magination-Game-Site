0b2b30222f38a50b3ff043384bd55286ff9fe1e4
Simen Vie <simenvie@gmail.com>
2016-05-03 18:16:05 +0200

    commented out fabric require as it is not in package.json

--- src/js/components/atoms/GameCreator.atom.js
+++ src/js/components/atoms/GameCreator.atom.js
@@ -1,19 +1,32 @@
 var React = require('react');
-
-var Fabric = require('fabric');
-
-var canvas = new Fabric.Canvas('fabricCanvas');
-
+var ReactDOM = require('react-dom');
+// var Fabric = require('fabric');
+var Col = require('react-bootstrap').Col;
+var canvas = new fabric.Canvas('fabricCanvas');
 var GameCreator = React.createClass({
+	getInitialState: function () {
+		return {
+			canvas: null
+		};
+	},
 	componentDidMount: function () {
-		Fabric.Image.fromURL('/public/img/magination-pieces/piece-white-single.png', function (oImg) {
-			canvas.add(oImg);
+		var parent = ReactDOM.findDOMNode(this.refs.canvasParent);
+		var canvas = ReactDOM.findDOMNode(this.refs.creatorCanvas);
+		canvas.width = parent.offsetWidth;
+		canvas.height = parent.offsetHeight;
+		this.setState({
+			canvas: new fabric.Canvas('fabricCanvas')
 		});
 	},
 	render: function () {
 		return (
-			<div>
-				<canvas id='fabricCanvas'></canvas>
+			<div style={{width: '100%', height: '100%'}}>
+				<Col md={4}>
+
+				</Col>
+				<Col md={8} ref='canvasParent'>
+					<canvas ref='creatorCanvas' id='fabricCanvas' style={{border: '1px solid black'}}></canvas>
+				</Col>
 			</div>
 		);
 	}
