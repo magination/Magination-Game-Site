34d3db79ef4384b3894e026ba3667f325bd915a8
Petter Iversen <iversen332@gmail.com>
2016-05-10 12:21:00 +0200

    style gamelist

--- src/js/components/molecules/browsegames/GameListElement.molecule.js
+++ src/js/components/molecules/browsegames/GameListElement.molecule.js
@@ -1,8 +1,10 @@
 var React = require('react');
 
 var TextStyles = require('../../../styles/Text');
-var Media = require('react-bootstrap').Media;
+var ContainerStyle = require('../../../styles/Containers');
+var Col = require('react-bootstrap').Col;
 var Glyphicon = require('react-bootstrap').Glyphicon;
+var CenteredImage = require('../../atoms/CenteredImage.atom');
 
 var GameListElement = React.createClass({
 	propTypes: {
@@ -15,35 +17,47 @@ var GameListElement = React.createClass({
 			owner: React.PropTypes.object.isRequired
 		})
 	},
+	getInitialState: function () {
+		return {
+			gameContainerStyle: ContainerStyle.gameList.regular
+		};
+	},
 	render: function () {
 		return (
-			<div>
-				<Media>
-					<Media.Left style={{cursor: 'pointer'}} onClick={this.onGameClick}>
-						<img width={200} height={200} src={this.props.game.images[0]} alt='No image' />
-					</Media.Left>
-					<Media.Body>
-						<h3 onClick={this.onGameClick} style={TextStyles.clickableHeader}>
-							{this.props.game.title}
-						</h3>
-						<h4>
-							<Glyphicon style={TextStyles.blue} glyph='star'/> {toOneDecimal(this.props.game.rating)}
-							<span style={{marginLeft: '30'}}/>
-							<Glyphicon style={TextStyles.blue} glyph='user'/> {this.props.game.numberOfPlayers}{(this.props.game.isPlayableWithMorePlayers) ? '+' : ''}
-						</h4>
-						<div onClick={this.onGameClick}>
-							<h4>Description:</h4>
-							<p>{this.props.game.shortDescription}</p>
-						</div>
-						<p>by <a style={{cursor: 'pointer'}}>{this.props.game.owner.username}</a></p>
-					</Media.Body>
-				</Media>
-				<hr/>
-			</div>
+			<Col ref='gameContainer' md={12} style={this.state.gameContainerStyle} onClick={this.onGameClick} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
+				<Col md={5}>
+					<CenteredImage src={this.props.game.images[0]}/>
+				</Col>
+				<Col md={7}>
+					<h3 onClick={this.onGameClick} style={TextStyles.clickableHeader}>
+						{this.props.game.title}
+					</h3>
+					<h4>
+						<Glyphicon style={TextStyles.blue} glyph='star'/> {toOneDecimal(this.props.game.rating)}
+						<span style={{marginLeft: '30'}}/>
+						<Glyphicon style={TextStyles.blue} glyph='user'/> {this.props.game.numberOfPlayers}{(this.props.game.isPlayableWithMorePlayers) ? '+' : ''}
+					</h4>
+					<div onClick={this.onGameClick}>
+						<h4>Description:</h4>
+						<p>{this.props.game.shortDescription}</p>
+					</div>
+					<p>by <a style={{cursor: 'pointer'}}>{this.props.game.owner.username}</a></p>
+				</Col>
+			</Col>
 		);
 	},
 	onGameClick: function () {
 		this.props.onGameClick(this.props.game._id);
+	},
+	onMouseOver: function () {
+		this.setState({
+			gameContainerStyle: ContainerStyle.gameList.hovered
+		});
+	},
+	onMouseOut: function () {
+		this.setState({
+			gameContainerStyle: ContainerStyle.gameList.regular
+		});
 	}
 });
 
