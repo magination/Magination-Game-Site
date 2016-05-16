475e9a9030ed7e161d99905d914ed697852b66cc
Petter Iversen <iversen332@gmail.com>
2016-05-16 13:42:16 +0200

    style and feedback

--- src/js/components/organisms/ConfirmForgotPassword.organism.js
+++ src/js/components/organisms/ConfirmForgotPassword.organism.js
@@ -1,41 +1,100 @@
 var React = require('react');
-
 var Input = require('react-bootstrap').Input;
 var Button = require('react-bootstrap').Button;
+var Col = require('react-bootstrap').Col;
+var Row = require('react-bootstrap').Row;
 
 var FeedbackAction = require('../../actions/FeedbackAction');
+var NavigationStore = require('../../stores/NavigationStore');
 
 var URLS = require('../../config/config').urls;
-var NavigationStore = require('../../stores/NavigationStore');
+var ButtonStyles = require('../../styles/Buttons');
+var TextStyles = require('../../styles/Text');
+
+var minPasswordLength = 7;
 
 var ConfirmForgotPassword = React.createClass({
 	getInitialState: function () {
 		return {
 			passwordEntryValue: '',
-			passwordConfirmEntryValue: ''
+			passwordConfirmEntryValue: '',
+			isFormValid: false
 		};
 	},
 	render: function () {
 		return (
 			<div>
-				<form onSubmit={this.onSubmit}>
-					<h2>Enter your new password</h2>
-					<Input placeholder='New Password' type='password' value={this.state.passwordEntryValue} onChange={this.onPasswordEntryChanged}/>
-					<Input placeholder='Confirm New Password' type='password' value={this.state.passwordConfirmEntryValue} onChange={this.onPasswordConfirmEntryChanged}/>
-					<Button type='submit'>Submit</Button>
-				</form>
+				<Col md={8} mdOffset={4}>
+					<form onSubmit={this.onSubmit}>
+						<Row>
+							<Col md={12}>
+								<h4>Enter your new password</h4>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={6}>
+								<Input
+									placeholder='New Password'
+									type='password'
+									value={this.state.passwordEntryValue}
+									onChange={this.onPasswordEntryChanged}
+									bsStyle={this.state.passwordBsStyle}
+									hasFeedback
+								/>
+							</Col>
+							<Col md={6}>
+								<h5 style={TextStyles.red}>
+									{this.state.passwordEntryValue.length >= minPasswordLength ? '' : 'Password must be at least 7 characters'}
+								</h5>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={6}>
+								<Input
+									placeholder='Confirm New Password'
+									type='password'
+									value={this.state.passwordConfirmEntryValue}
+									onChange={this.onPasswordConfirmEntryChanged}
+									bsStyle={this.state.passwordConfirmBsStyle}
+									hasFeedback
+								/>
+							</Col>
+							<Col md={6}>
+								<h5 style={TextStyles.red}>
+									{this.state.passwordEntryValue === this.state.passwordConfirmEntryValue ? '' : 'Passwords must match'}
+								</h5>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={6}>
+								<Button
+									type='submit'
+									disabled={this.state.isFormValid}
+									style={ButtonStyles.MaginationFillParent}
+								>
+									Submit
+								</Button>
+							</Col>
+						</Row>
+					</form>
+				</Col>
 			</div>
 		);
 	},
 	onPasswordEntryChanged: function (e) {
 		this.setState({
-			passwordEntryValue: e.target.value
+			passwordEntryValue: e.target.value,
+			passwordBsStyle: e.target.value.length >= minPasswordLength ? 'success' : 'error',
+			passwordConfirmBsStyle: e.target.value === this.state.password && e.target.value.length >= minPasswordLength ? 'success' : 'error'
 		});
+		this.validateForm();
 	},
 	onPasswordConfirmEntryChanged: function (e) {
 		this.setState({
-			passwordConfirmEntryValue: e.target.value
+			passwordConfirmEntryValue: e.target.value,
+			passwordConfirmBsStyle: e.target.value === this.state.passwordEntryValue && e.target.value.length >= minPasswordLength ? 'success' : 'error'
 		});
+		this.validateForm();
 	},
 	onSubmit: function (e) {
 		e.preventDefault();
@@ -64,6 +123,9 @@ var ConfirmForgotPassword = React.createClass({
 			header: 'Error',
 			message: 'The request url is either expired or does not exist, please request a new email'
 		});
+	},
+	validateForm: function () {
+		return this.state.passwordEntryValue >= minPasswordLength && this.state.passwordEntryValue === this.state.passwordConfirmEntryValue;
 	}
 });
 
