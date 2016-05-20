2bd0afbb950ad1f0b6466ceda41e483e935d13af
Petter Iversen <iversen332@gmail.com>
2016-05-20 18:54:31 +0200

    refactor

--- src/js/components/atoms/InputWithTooltip.js
+++ src/js/components/atoms/InputWithTooltip.js
@@ -1,3 +1,27 @@
-/**
- * Created by petteriversen on 15.05.16.
- */
+var React = require('react');
+var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
+var Tooltip = require('react-bootstrap').Tooltip;
+var Input = require('react-bootstrap').Input;
+
+var ConfirmButton = React.createClass({
+	render: function () {
+		return (
+			<OverlayTrigger
+                trigger='focus'
+                placement={this.props.placement}
+                overlay={<Tooltip ref='tooltip'>{this.props.tooltip}</Tooltip>}
+            >
+                <Input
+                    value={this.props.value}
+                    bsStyle={this.props.bsStyle}
+                    label={this.props.label}
+                    type={this.props.type}
+                    placeholder={this.props.placeholder}
+                    onChange={this.props.onChange}
+                    hasFeedback={this.props.hasFeedback}
+                />
+			</OverlayTrigger>
+		);
+	}
+});
+module.exports = ConfirmButton;
