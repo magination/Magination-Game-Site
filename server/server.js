435b1d0e6d76c43ba1e143bb086d2329c73bb20a
Tibor Vukovic <tiborvukovic91@gmail.com>
2016-09-03 13:41:18 +0200

    Add react prod env and update lint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ var https = require('https');
 var fs = require('fs');
 var app = express();
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use('/public', express.static(path.join(__dirname, '../build')));
 
 app.get('*', function (req, res) {
@@ -19,4 +26,3 @@ https.createServer(options, app).listen(8080, function (err) {
 	if (err) console.log(err);
 	else console.log('Server listening...');
 });
-
